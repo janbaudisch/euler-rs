4150fa41d82d9e11205b4487164b2d3a35e624e0
Jan Baudisch <dev@baudisch.xyz>
2019-01-26 19:24:10 +0100

    add: problem 19

---
+++ problem_19/src/date.rs
@@ -0,0 +1,109 @@
+pub struct Date {
+    pub weekday: Weekday,
+    pub day: u8,
+    pub month: Month,
+    pub year: u16
+}
+
+impl Date {
+    pub fn next(self) -> Self {
+        let weekday = self.weekday.next();
+        let mut day = self.day + 1;
+        let mut month = self.month;
+
+        if day > 28 && self.month == Month::February {
+            if (self.year % 4 == 0 && self.year % 100 != 0) || self.year % 400 == 0 {
+                if day == 30 {
+                    month = month.next();
+                    day = 1;
+                }
+            } else {
+                month = month.next();
+                day = 1;
+            }
+        } else if (day == 31 && self.month.is_short()) || (day == 32 && !self.month.is_short()) {
+            month = month.next();
+            day = 1;
+        }
+
+        let year = if month == Month::January && day == 1 {
+            self.year + 1
+        } else {
+            self.year
+        };
+
+        Self {
+            weekday,
+            day,
+            month,
+            year
+        }
+    }
+}
+
+#[derive(PartialEq)]
+pub enum Weekday {
+    Monday,
+    Tuesday,
+    Wednesday,
+    Thursday,
+    Friday,
+    Saturday,
+    Sunday
+}
+
+impl Weekday {
+    pub fn next(self) -> Weekday {
+        match self {
+            Weekday::Monday => Weekday::Tuesday,
+            Weekday::Tuesday => Weekday::Wednesday,
+            Weekday::Wednesday => Weekday::Thursday,
+            Weekday::Thursday => Weekday::Friday,
+            Weekday::Friday => Weekday::Saturday,
+            Weekday::Saturday => Weekday::Sunday,
+            Weekday::Sunday => Weekday::Monday
+        }
+    }
+}
+
+#[derive(Clone, Copy, PartialEq)]
+pub enum Month {
+    January,
+    February,
+    March,
+    April,
+    May,
+    June,
+    Juli,
+    August,
+    September,
+    October,
+    November,
+    December
+}
+
+impl Month {
+    pub fn next(self) -> Month {
+        match self {
+            Month::January => Month::February,
+            Month::February => Month::March,
+            Month::March => Month::April,
+            Month::April => Month::May,
+            Month::May => Month::June,
+            Month::June => Month::Juli,
+            Month::Juli => Month::August,
+            Month::August => Month::September,
+            Month::September => Month::October,
+            Month::October => Month::November,
+            Month::November => Month::December,
+            Month::December => Month::January
+        }
+    }
+
+    pub fn is_short(self) -> bool {
+        self == Month::April
+            || self == Month::June
+            || self == Month::September
+            || self == Month::November
+    }
+}
---
+++ problem_19/src/main.rs
@@ -0,0 +1,36 @@
+mod date;
+
+use date::*;
+
+fn main() {
+    let mut current = Date {
+        weekday: Weekday::Monday,
+        day: 1,
+        month: Month::January,
+        year: 1900
+    };
+
+    let mut count = 0;
+
+    loop {
+        current = current.next();
+
+        if current.year > 1900 {
+            break;
+        }
+    }
+
+    loop {
+        if current.day == 1 && current.weekday == Weekday::Sunday {
+            count += 1;
+        }
+
+        current = current.next();
+
+        if current.year > 2000 {
+            break;
+        }
+    }
+
+    println!("[SOLUTION] {} Sundays on the first of the month", count);
+}
