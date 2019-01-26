e2fc659ca292aeebc705dd735b6496c61cccfec3
Jan Baudisch <dev@baudisch.xyz>
2019-01-26 21:21:17 +0100

    add: problem 35

---
+++ problem_035/src/main.rs
@@ -0,0 +1,34 @@
+use common::input;
+use common::math::IsPrime;
+
+fn main() {
+    println!("[INPUT] calculate primes until:");
+    let limit = u64::from_str_radix(&input::read_line(), 10).expect("Could not parse input!");
+
+    let mut count = 0;
+
+    for n in 2..limit {
+        if n.is_prime() {
+            let mut digits = n.to_string().chars().into_iter().collect::<Vec<char>>();
+
+            let mut flag = true;
+
+            for _ in 0..digits.len() {
+                digits.rotate_right(1);
+                let x =
+                    u64::from_str_radix(&digits.clone().iter().collect::<String>(), 10).unwrap();
+
+                if !x.is_prime() {
+                    flag = false;
+                    break;
+                }
+            }
+
+            if flag {
+                count += 1;
+            }
+        }
+    }
+
+    println!("[SOLUTION] circular primes below {}: {}", limit, count);
+}
